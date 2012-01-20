69698aa0f262a9bb85b3851b9af0fc7adb1df4c4
Lucas Cimon <lucas.cimon@ensimag.fr>
2012-01-20 17:41:03 +0100

    implement exp mean

--- OpenNiViewer/SlidingWindow.h
+++ OpenNiViewer/SlidingWindow.h
@@ -3,15 +3,15 @@
 
 #include <deque>
 
-class SlidingWindow
+class SlidingWindowMean
 {
 private:
 	int windowSize_;
-	std::deque<double> memory_;
 	double updatedMean_;
+	std::deque<double> memory_;
 
 public:
-	SlidingWindow(int windowSize) : windowSize_(windowSize) {}
+	SlidingWindowMean(int windowSize) : windowSize_(windowSize), updatedMean_(0), memory_() {}
 
 	void push(double value) {
 		const unsigned int memSize = memory_.size();
@@ -27,5 +27,29 @@ public:
 
 	double mean() { return updatedMean_; }
 };
+
+class SlidingWindowExp
+{
+private:
+	double mean_;
+	int nbValues_;
+	double alpha_;
+
+public:
+	SlidingWindowExp(double alpha) : mean_(0), nbValues_(0), alpha_(alpha) {}
+
+	void push(double value) {
+		mean_ = (1 - alpha_) * mean_ + alpha_ * value;
+	}
+
+	double mean() { return mean_; }
+};
+
+#ifdef USE_SLIDING_WINDOW_EXP
+typedef SlidingWindowMean SlidingWindow;
+#else
+typedef SlidingWindowExp SlidingWindow;
+#endif
+
 
 #endif //DEF_H_SLIDING_WINDOW_
